04814cd36deeb88979ab2d4d5185d53176265371
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-04-12 21:44:25 +0500

    fix: fixed during testing

--- service/auth/AuthService.js
+++ service/auth/AuthService.js
@@ -31,7 +31,9 @@ export async function login(req, res) {
         return res.status(400)
             .json({success: false, message: "Invalid email"});
     }
-    const user = await User.findOne({ email: email });
+    const user = await User.findOne({
+        where: { email: email }
+    });
     if (!user) {
         return res.status(400)
         .json({success: false, message: "User not found"});
@@ -49,7 +51,9 @@ export async function login(req, res) {
 export async function register(req, res) {
     let message = {};
     const { email, unique_code, password } = req.body;
-    const isUserExists = await User.findOne({ email: email });
+    const isUserExists = await User.findOne({
+        where: { email: email }
+    });
     if (isUserExists) {
         return res.status(400)
             .json({success: false, message: "User already exists"});
