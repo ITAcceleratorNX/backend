bb8860c642a87fdc236072acc3d0553d7fd01f5c
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-05-07 23:34:32 +0500

    fix: fixed tag of chat endpoints

--- routes/faq/FAQRoutes.js
+++ routes/faq/FAQRoutes.js
@@ -2,7 +2,7 @@ import express from "express";
 import {validateBody} from "../../middleware/validate.js";
 import {FAQDto, UpdateFAQDto} from "../../dto/faq/FAQ.dto.js";
 import { authorizeAdmin} from "../../middleware/jwt.js";
-import * as FAQController from "../../controllers/faq/FAQController";
+import * as FAQController from "../../controllers/faq/FAQController.js";
 
 const router = express.Router();
 
