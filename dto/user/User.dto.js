c83f72024c78cc5ac53bce536302cca98388611e
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-05-12 01:24:31 +0500

    fix: fixed user dto

--- dto/user/User.dto.js
+++ dto/user/User.dto.js
@@ -2,7 +2,7 @@ import { z } from 'zod';
 
 export const UserDto = z.object({
     name: z.string().min(1),
-    email: z.string().length(12),
+    email: z.string().email("Invalid email address"),
     phone: z.string().min(1),
     iin: z.string().min(12).max(12)
 });
