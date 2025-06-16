709cb9a55a1fdb7729682fa13f24949e20f3bdb2
damirablahat <damirablahat0@gmail.com>
2025-06-16 19:16:49 +0500

    fix: fix chat

--- src/main/models/Message.js
+++ src/main/models/Message.js
@@ -2,18 +2,19 @@ import {sequelize} from "../config/database.js";
 import {DataTypes} from "sequelize";
 
 export const Message = sequelize.define('Message', {
-        id: {
-            type: DataTypes.INTEGER,
-            autoIncrement: true,
-            primaryKey: true
-        },
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
     chat_id: DataTypes.INTEGER,
     sender_id: DataTypes.INTEGER,
     text: DataTypes.STRING,
-    is_from_user: DataTypes.BOOLEAN
-},
-    {
-        tableName: 'messages',
-        timestamps: false
-    });
+    is_from_user: DataTypes.BOOLEAN,
+}, {
+    tableName: 'messages',
+    timestamps: true,
+    createdAt: 'created_at',
+    updatedAt: false
+});
 export default Message;
