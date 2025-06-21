10345d0d1f3a0694c8b4e6d701caf204ccb95280
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-06-21 22:49:30 +0500

    fix: date when saving transaction

--- src/main/service/callback/PaymentCallback.service.js
+++ src/main/service/callback/PaymentCallback.service.js
@@ -217,6 +217,7 @@ const processManualErrorAndNotify = async (data, title, message) => {
 
 export const processCronJobForExpiredTransactions = async () => {
     const PAYMENT_LIFETIME = Number(process.env.PAYMENT_LIFETIME);
+    console.log("PAYMENT_LIFETIME: ", PAYMENT_LIFETIME);
     const expirationCutoff = new Date(Date.now() - PAYMENT_LIFETIME * 1000);
 
     const expiredTransactions = await Transaction.findAll({
