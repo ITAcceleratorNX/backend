0fb6e57ab2fae053670157f1fb4dc766080ca790
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-07-21 13:37:11 +0500

    feat: logging

--- src/main/utils/winston/logger.js
+++ src/main/utils/winston/logger.js
@@ -21,8 +21,8 @@ const transport = new DailyRotateFile({
 const consoleFormat = winston.format.combine(
     winston.format.colorize({ all: true }),
     winston.format.timestamp(),
-    winston.format.printf(({ timestamp, level, message }) => {
-        return `[${timestamp}] ${level}: ${message}`;
+    winston.format.printf(({ timestamp, level, message, ...meta }) => {
+        return `[${timestamp}] ${level}: ${message} ${Object.keys(meta).length ? JSON.stringify(meta, null, 2) : ''}`;
     })
 );
 
