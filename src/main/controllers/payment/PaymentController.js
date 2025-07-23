e59e3dbb5e7e6db0547f345e0899e3459566bde1
ZhubanyshZh <zubanyszarylkasyn@gmail.com>
2025-07-23 22:14:41 +0500

    feat: redirecting to payment page url

--- src/main/controllers/payment/PaymentController.js
+++ src/main/controllers/payment/PaymentController.js
@@ -4,7 +4,7 @@ import {asyncHandler} from "../../utils/handler/asyncHandler.js";
 
 export const createPayment = asyncHandler(async (req, res) => {
     const response = await paymentService.create(req.body, req.user.id);
-    res.status(201).json(response);
+    res.redirect(response.payment_page_url);
 });
 
 export const getMyPayments = asyncHandler(async (req, res) => {
